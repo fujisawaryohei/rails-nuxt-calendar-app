3a579336c601d8e889a9d519933dc0b7c8a4b7df
fujisawaryohei <ryoheifujisawa@i.softbank.jp>
2020-11-19 23:32:05 +0900

    余計なファイルの削除

---
+++ frontend/components/SideNav.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    :mini-variant="miniVariant"
+    :clipped="clipped"
+    fixed
+    app
+  >
+    <v-list>
+      <v-list-item
+        v-for="(item, i) in items"
+        :key="i"
+        :to="item.to"
+        router
+        exact
+      >
+        <v-list-item-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.title" />
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script>
+export default {
+  props: {
+    drawer: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    miniVariant: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    clipped: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    items: {
+      type: Array,
+      required: true,
+      default: () => {
+        return []
+      },
+    },
+  },
+}
+</script>
+s
--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,29 +1,11 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
+    <side-nav-menu
+      :drawer="drawer"
       :mini-variant="miniVariant"
       :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+      :items="loggedInItems"
+    />
     <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-btn icon @click.stop="miniVariant = !miniVariant">
@@ -32,30 +14,14 @@
       <v-btn icon @click.stop="clipped = !clipped">
         <v-icon>mdi-application</v-icon>
       </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
@@ -63,28 +29,26 @@
 </template>
 
 <script>
+import SideNavMenu from '../components/SideNav'
 export default {
+  components: {
+    'side-nav-menu': SideNavMenu,
+  },
   data() {
     return {
       clipped: false,
       drawer: false,
       fixed: false,
-      items: [
+      loggedInItems: [
         {
           icon: 'mdi-apps',
-          title: 'Welcome',
+          title: 'カレンダー',
           to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
         },
       ],
       miniVariant: false,
       right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
+      title: 'Roryu',
     }
   },
 }
