035ddaf459cb3386f35c6caa0cc772c7f339be08
fujisawaryohei <ryoheifujisawa@i.softbank.jp>
2020-11-29 19:44:45 +0900

    完了ステータス処理追加

--- frontend/components/TodoCard.vue
+++ frontend/components/TodoCard.vue
@@ -9,7 +9,10 @@
           <v-spacer></v-spacer>
           <v-btn depressed color="error" @click="remove()"> 削除する </v-btn>
           <v-btn depressed color="info" @click="toEdit()"> 編集する </v-btn>
-          <v-btn depressed color="primary"> 完了 </v-btn>
+          <v-btn v-if="!todo.is_done" depressed color="primary" @click="done()">
+            完了
+          </v-btn>
+          <v-btn v-else depressed color="warning" @click="back()">未完了</v-btn>
         </v-card-actions>
       </v-card>
     </v-col>
@@ -41,6 +44,20 @@ export default {
   methods: {
     toEdit() {
       this.$router.push(`/todos/${this.todo.id}`)
+    },
+    async done() {
+      await this.$axios.$patch(`/api/v1/todos/${this.todo.id}`, {
+        is_done: true,
+      })
+      // TODO: 環境変数管理にシフト
+      window.location.href = `http://localhost:3000/todos`
+    },
+    async back() {
+      await this.$axios.$patch(`/api/v1/todos/${this.todo.id}`, {
+        is_done: false,
+      })
+      // TODO: 環境変数管理にシフト
+      window.location.href = `http://localhost:3000/todos`
     },
     async remove() {
       const confirmation = window.confirm('本当に削除しますか？')
