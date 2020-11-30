36b5efc71e2d3500577acb89f7a028119d110ad7
fujisawaryohei <ryoheifujisawa@i.softbank.jp>
2020-11-30 16:36:53 +0900

    修正

--- frontend/components/TodoCard.vue
+++ frontend/components/TodoCard.vue
@@ -9,10 +9,17 @@
           <v-spacer></v-spacer>
           <v-btn depressed color="error" @click="remove()"> 削除する </v-btn>
           <v-btn depressed color="info" @click="toEdit()"> 編集する </v-btn>
-          <v-btn v-if="!todo.is_done" depressed color="primary" @click="done()">
+          <v-btn
+            v-if="!todo.is_done"
+            depressed
+            color="primary"
+            @click="complete()"
+          >
             完了
           </v-btn>
-          <v-btn v-else depressed color="warning" @click="back()">未完了</v-btn>
+          <v-btn v-else depressed color="warning" @click="incomplete()"
+            >未完了</v-btn
+          >
         </v-card-actions>
       </v-card>
     </v-col>
@@ -45,13 +52,13 @@ export default {
     toEdit() {
       this.$router.push(`/todos/${this.todo.id}`)
     },
-    async done() {
+    async complete() {
       await this.$axios.$patch(`/api/v1/todos/${this.todo.id}`, {
         is_done: true,
       })
       window.location.href = process.env.hostUrl + '/todos'
     },
-    async back() {
+    async incomplete() {
       await this.$axios.$patch(`/api/v1/todos/${this.todo.id}`, {
         is_done: false,
       })
