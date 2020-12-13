f1980a9949e3d5cd837f1e8d3a232d99625b6c96
fujisawaryohei <ryoheifujisawa@i.softbank.jp>
2020-12-13 21:09:42 +0900

    一旦コミット

--- server/CloudFormation2/create-stack.sh
+++ server/CloudFormation2/create-stack.sh
@@ -3,6 +3,12 @@ set -eu
 # VPCスタックテンプレートの検証
 aws cloudformation validate-template --template-body file://"$(pwd)"/Stack/VPC.yml
 
+# ELBスタックテンプレートの検証
+aws cloudformation validate-template --template-body file://"$(pwd)"/Stack/ElasticLoadBalancing.yml
+
+# RDSスタックテンプレートの検証
+aws cloudformation validate-template --template-body file://"$(pwd)"/Stack/RDS.yml
+
 # TaskDefnitionスタックテンプレートの検証
 aws cloudformation validate-template --template-body file://"$(pwd)"/Stack/ElasticContainerService/TaskDefinition.yml
 
@@ -13,24 +19,32 @@ aws cloudformation validate-template --template-body file://"$(pwd)"/Stack/Elast
 aws cloudformation validate-template --template-body file://"$(pwd)"/Stack/ElasticContainerService/Service.yml
 
 # Route53スタックテンプレートの検証 TODO: Fargate IPの固定化
-# aws cloudformation validate-template --template-body file://"$(pwd)"/Stack/Route53.yml
+aws cloudformation validate-template --template-body file://"$(pwd)"/Stack/Route53.yml
 
 # VPCスタックの作成
-aws cloudformation create-stack --stack-name VPCStack --template-body file://"$(pwd)"/Stack/VPC.yml
+aws cloudformation create-stack --stack-name VPCStack --template-body file://"$(pwd)"/Stack/VPC.yml --profile fusic_dev
 aws cloudformation wait stack-create-complete --stack-name VPCStack
+
+# ELBスタックの作成
+aws cloudformation create-stack --stack-name ELBStack --template-body file://"$(pwd)"/Stack/ElasticLoadBalancing.yml --profile fusic_dev
+aws cloudformation wait stack-create-complete --stack-name ELBStack
+
+# RDSスタックの作成
+aws cloudformation create-stack --stack-name RDSStack --template-body file://"$(pwd)"/Stack/RDS.yml --profile fusic_dev
+aws cloudformation wait stack-create-complete --stack-name RDSStack
 
 # TaskDefnitionスタックの作成
 # capabilitiesオプション参考: https://docs.aws.amazon.com/ja_jp/AWSCloudFormation/latest/UserGuide/using-iam-template.html#using-iam-capabilities
-aws cloudformation create-stack --stack-name TaskDefinitionStack --template-body file://"$(pwd)"/Stack/ElasticContainerService/TaskDefinition.yml --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation create-stack --stack-name TaskDefinitionStack --template-body file://"$(pwd)"/Stack/ElasticContainerService/TaskDefinition.yml --capabilities CAPABILITY_NAMED_IAM --profile fusic_dev
 aws cloudformation wait stack-create-complete --stack-name TaskDefinitionStack
 
 # Clusterスタックの作成
-aws cloudformation create-stack --stack-name ClusterStack --template-body file://"$(pwd)"/Stack/ElasticContainerService/Cluster.yml
+aws cloudformation create-stack --stack-name ClusterStack --template-body file://"$(pwd)"/Stack/ElasticContainerService/Cluster.yml --profile fusic_dev
 aws cloudformation wait stack-create-complete --stack-name ClusterStack
 
 # Serviceスタックの作成
-aws cloudformation create-stack --stack-name ServiceStack --template-body file://"$(pwd)"/Stack/ElasticContainerService/Service.yml
+aws cloudformation create-stack --stack-name ServiceStack --template-body file://"$(pwd)"/Stack/ElasticContainerService/Service.yml --profile fusic_dev
 aws cloudformation wait stack-create-complete --stack-name ServiceStack
 
 # Route53スタックの作成 TODO: Fargate IPの固定化
-# aws cloudformation create-stack --stack-name Route53Stack --template-body file://"$(pwd)"/Stack/Route53.yml
+# aws cloudformation create-stack --stack-name Route53Stack --template-body file://"$(pwd)"/Stack/Route53.yml --profile fusic_dev
--- server/CloudFormation2/update-stack.sh
+++ server/CloudFormation2/update-stack.sh
@@ -3,6 +3,9 @@ set -eu
 # VPCスタックテンプレートの検証
 aws cloudformation validate-template --template-body file://"$(pwd)"/Stack/VPC.yml
 
+# RDSスタックテンプレートの検証
+aws cloudformation validate-template --template-body file://"$(pwd)"/Stack/RDS.yml
+
 # TaskDefnitionスタックテンプレートの検証
 aws cloudformation validate-template --template-body file://"$(pwd)"/Stack/ElasticContainerService/TaskDefinition.yml
 
@@ -15,22 +18,26 @@ aws cloudformation validate-template --template-body file://"$(pwd)"/Stack/Elast
 # Route53スタックテンプレートの検証 TODO: Fargate IPの固定化
 # aws cloudformation validate-template --template-body file://"$(pwd)"/Route53.yml
 
-# VPCスタックの作成
+# VPCスタックの更新
 aws cloudformation update-stack --stack-name VPCStack --template-body file://"$(pwd)"/Stack/VPC.yml
 aws cloudformation wait stack-update-complete --stack-name VPCStack
 
-# TaskDefnitionスタックの作成
+# RDSスタックの更新
+aws cloudformation update-stack --stack-name RDSStack --template-body file://"$(pwd)"/Stack/RDS.yml
+aws cloudformation wait stack-update-complete --stack-name RDSStack
+
+# TaskDefnitionスタックの更新
 # capabilitiesオプション参考: https://docs.aws.amazon.com/ja_jp/AWSCloudFormation/latest/UserGuide/using-iam-template.html#using-iam-capabilities
 aws cloudformation update-stack --stack-name TaskDefinitionStack --template-body file://"$(pwd)"/Stack/ElasticContainerService/TaskDefinition.yml --capabilities CAPABILITY_NAMED_IAM
 aws cloudformation wait stack-update-complete --stack-name TaskDefinitionStack
 
-# Clusterスタックの作成
+# Clusterスタックの更新
 aws cloudformation update-stack --stack-name ClusterStack --template-body file://"$(pwd)"/Stack/ElasticContainerService/Cluster.yml
 aws cloudformation wait stack-update-complete --stack-name ClusterStack
 
-# Serviceスタックの作成
+# Serviceスタックの更新
 aws cloudformation update-stack --stack-name ServiceStack --template-body file://"$(pwd)"/Stack/ElasticContainerService/Service.yml
 aws cloudformation wait stack-update-complete --stack-name ServiceStack
 
-# Route53スタックの作成 TODO: Fargate IPの固定化
+# Route53スタックの更新 TODO: Fargate IPの固定化
 # aws cloudformation update-stack --stack-name Route53Stack --template-body file://"$(pwd)"/Stack/Route53.yml
